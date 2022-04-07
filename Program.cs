b75224059d5b899fca55fdb4143de07e0c8bd70e
Afetzner <59979332+Afetzner@users.noreply.github.com>
2022-04-07 15:07:48 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CSCE361_voting_system
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
