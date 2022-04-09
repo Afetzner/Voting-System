ae6a7800e156be424afb7c6f336eee2e4532f273
Afetzner <59979332+Afetzner@users.noreply.github.com>
2022-04-09 17:03:38 -0500

    Added DB connection. Added regex matching for names

--- Program.cs
+++ Program.cs
@@ -1,11 +1,16 @@
 ﻿using System;
+using System.Text.RegularExpressions;
+using CSCE361_voting_system.SQL;
 
 namespace CSCE361_voting_system.Model
 {
     class Program
     {
+
+        
         static void Main(string[] args)
         {
+
         }
     }
 }
