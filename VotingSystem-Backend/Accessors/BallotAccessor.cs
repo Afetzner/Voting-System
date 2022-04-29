d68a03da659933a36819d278f06c85b6754560fd
akrestovsky <59978361+akrestovsky@users.noreply.github.com>
2022-04-29 15:37:44 -0500

    Ballot Model Class updated, Ballot Accessor in progress (non-compiling)

--- VotingSystem-Backend/Accessors/BallotAccessor.cs
+++ VotingSystem-Backend/Accessors/BallotAccessor.cs
@@ -1,33 +1,107 @@
 ﻿using VotingSystem.Accessor;
+using System;
+using VotingSystem.Model;
+using System.Data;
+using MySql.Data.MySqlClient;
 
 namespace VotingSystem.Model
 {
     public class BallotAccessor : IBallotAccessor
     {
         //TODO
-        public bool AddBallot(Ballot ballot)
+public bool AddBallot(Ballot ballot)
+{
+            using (var conn = new MySqlConnection(DbConnecter.ConnectionString))
+            {
+                conn.Open();
+            }
+            catch(MySqlException e)
+            {
+                Console.WriteLine(e + "\nCould not connect to database");
+                throw;
+            }
+
+            using (var cmd = new MySqlCommand("add_ballot"))
+            {
+                cmd.CommandType = CommandType.StoredProcedure;
+                /*
+                IN `v_ballotSerial` varchar(9),  
+                IN `v_voterSerial` varchar(9),
+                IN `v_issueSerial` varchar(9),
+                IN `v_choiceNumber` int,
+                OUT `v_collision` bool)*/
+                cmd.Parameters.AddWithValue("v_ballotSerial", ballot.)
+            }
+      
+    throw new NotImplementedException();
+}
+
+public List<Ballot> GetBallotsByVoter(string voterSerial)
+{
+            /*
+           CREATE PROCEDURE afetzner.get_voters_ballot(
+    IN `v_voterSerial` varchar(9),
+    IN `v_issueSerial` varchar(9),
+    OUT `v_ballotSerial` varchar(9),
+    OUT `v_choiceNumber` int,
+    OUT `v_choiceTitle` varchar(127))
+BEGIN
+  END
+                */
+    using(var conn = new MySqlConnection(DbConnecter.ConnectionString))
+            {
+                try
+                {
+                    conn.Open();
+                }
+                catch (MySqlException e)
+                {
+                    Console.WriteLine(e + "\nCould not connect to database");
+                    throw;
+                }
+
+                using (var cmd = new MySqlCommand("get_voters_ballot", conn))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+
+                    cmd.Parameters.AddWithValue("v_voterSerial", voterSerial);
+                    cmd.Parameters["v_voterSerial"].Direction = ParameterDirection.Input
+                }
+            }
+    throw new NotImplementedException();
+}
+
+public List<BallotIssue> GetIssuesVotedOn(string voterSerial)
+{
+    throw new NotImplementedException();
+}
+
+public bool IsSerialInUse(string ballotSerial)
+{
+    using (var conn = new MySqlConnection(DbConnecter.ConnectionString))
+    {
+        try
         {
-            throw new NotImplementedException();
+            conn.Open();
+        }
+        catch (MySqlException e)
+        {
+            Console.WriteLine(e + "\nCound not connect to database");
+            throw;
         }
 
-        public List<Ballot> GetBallotsByVoter(string voterSerial)
-        {
-            throw new NotImplementedException();
-        }
+        using (var cmd = new MySqlCommand("check_ballot_serial")
+    /*
+    CREATE PROCEDURE afetzner.check_ballot_serial(
+    IN `v_ballotSerial` varchar(9),
+    OUT `v_occupied` bool)
+    */
+    throw new NotImplementedException();
+}
 
-        public List<BallotIssue> GetIssuesVotedOn(string voterSerial)
-        {
-            throw new NotImplementedException();
-        }
-
-        public bool IsSerialInUse(string ballotSerial)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void RemoveBallot(string serial)
-        {
-            throw new NotImplementedException();
-        }
-    }
+public void RemoveBallot(string serial)
+{
+    throw new NotImplementedException();
+}
+}
 }
--- VotingSystem-Backend/Models/Ballot.cs
+++ VotingSystem-Backend/Models/Ballot.cs
@@ -9,14 +9,16 @@ namespace VotingSystem.Model
         public Voter Voter { get; }
         public BallotIssue Issue { get; }
         public BallotIssueOption Choice { get; }
+        public String SerialNumber { get; }
 
         public static readonly IBallotAccessor Accessor = new BallotAccessor();
 
-        public Ballot(Voter voter, BallotIssue issue, BallotIssueOption choice)
+        public Ballot(Voter voter, BallotIssue issue, BallotIssueOption choice, String serialNumber)
         {
             Voter = voter;
             Issue = issue;
             Choice = choice;
+            SerialNumber = serialNumber;
         }
     }
 
@@ -25,6 +27,7 @@ namespace VotingSystem.Model
             public Voter? Voter;
             public BallotIssue? Issue;
             public BallotIssueOption? Choice;
+            public String? SerialNumber;
             private bool _inputtedChoice;
 
             public BallotBuilder WithVoter(Voter voter)
@@ -46,16 +49,24 @@ namespace VotingSystem.Model
                 return this;
             }
 
+            public BallotBuilder WithSerialNumber(String serialNumber)
+            {
+                SerialNumber = serialNumber;
+                return this;
+            }
+
             public Ballot Build()
             {
                 if (Voter == null)
                     throw new InvalidBuilderParameterException("Invalid (Null) BallotVoter");
                 if (Issue == null)
                     throw new InvalidBuilderParameterException("Invalid (null) BallotIssue");
+                if (SerialNumber == null)
+                    throw new InvalidBuilderParameterException("Invalid (null) SerialNumber");
                 if (!_inputtedChoice || Choice == null)
                     throw new InvalidBuilderParameterException(
                         "Invalid BallotIssue choice (Null allowed, but must be inputted: WithChoice(null))");
-                Ballot ballot = new (Voter, Issue, Choice);
+                Ballot ballot = new (Voter, Issue, Choice, SerialNumber);
                 return ballot;
             }
         }
