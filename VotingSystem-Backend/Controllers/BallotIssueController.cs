648d0c7c20d531024cabdc4c9f04b22af2d65e85
Alex Busch <69646582+abusch8@users.noreply.github.com>
2022-04-30 16:25:00 -0500

    Fixed merge conflicts

--- VotingSystem-Backend/Controllers/BallotIssueController.cs
+++ VotingSystem-Backend/Controllers/BallotIssueController.cs
@@ -13,59 +13,7 @@ namespace VotingSystem.Controller
         [Route("api/polls")]
         public List<BallotIssue> GetBallotIssues()
         {
-<<<<<<< HEAD
-            /*
-            List<BallotIssue> list = new List<BallotIssue>();
-            BallotIssue issue1 = new BallotIssue.BallotIssueBuilder()
-                .WithSerialNumber("A12345678")
-                .WithStartDate(new DateTime(2022, 3, 12))
-                .WithEndDate(new DateTime(2022, 8, 5))
-                .WithTitle("Test v. Test")
-                .WithDescription("test")
-                .WithOptions("option1", "option2")
-                .Build();
-            list.Add(issue1);
-            BallotIssue issue2 = new BallotIssue.BallotIssueBuilder()
-                .WithSerialNumber("B12345678")
-                .WithStartDate(new DateTime(2022, 3, 12))
-                .WithEndDate(new DateTime(2022, 7, 5))
-                .WithTitle("Test v. Test")
-                .WithDescription("test")
-                .WithOptions("option1", "option2")
-                .Build();
-            list.Add(issue2);
-            BallotIssue issue3 = new BallotIssue.BallotIssueBuilder()
-                .WithSerialNumber("C12345678")
-                .WithStartDate(new DateTime(2022, 3, 12))
-                .WithEndDate(new DateTime(2022, 7, 5))
-                .WithTitle("Test v. Test")
-                .WithDescription("test")
-                .WithOptions("option1", "option2")
-                .Build();
-            list.Add(issue3);
-            BallotIssue issue4 = new BallotIssue.BallotIssueBuilder()
-                .WithSerialNumber("D12345678")
-                .WithStartDate(new DateTime(2022, 3, 12))
-                .WithEndDate(new DateTime(2022, 8, 5))
-                .WithTitle("Test v. Test")
-                .WithDescription("test")
-                .WithOptions("option1", "option2")
-                .Build();
-            list.Add(issue4);
-            BallotIssue issue5 = new BallotIssue.BallotIssueBuilder()
-                .WithSerialNumber("E12345678")
-                .WithStartDate(new DateTime(2022, 3, 12))
-                .WithEndDate(new DateTime(2022, 8, 5))
-                .WithTitle("Test v. Test")
-                .WithDescription("test")
-                .WithOptions("option1", "option2")
-                .Build();
-            list.Add(issue5);
-            */
-            return BallotIssue.Accessor.GetBallotIssues();
-=======
             return BallotIssue.Accessor.GetBallotIssues(); 
->>>>>>> ad412585677c46443e2ead4d110df4a6d69ed8a0
         }
     }
 }
