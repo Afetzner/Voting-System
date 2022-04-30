ea3df6ca922d34eff701647d54510ef5fbd9f0b2
akrestovsky <59978361+akrestovsky@users.noreply.github.com>
2022-04-29 19:11:51 -0500

    Update BallotAccessor.cs

--- VotingSystem-Backend/Accessors/BallotAccessor.cs
+++ VotingSystem-Backend/Accessors/BallotAccessor.cs
@@ -1,4 +1,4 @@
-﻿using VotingSystem.Accessor;
+using VotingSystem.Accessor;
 using System;
 using VotingSystem.Model;
 using System.Data;
@@ -8,74 +8,64 @@ namespace VotingSystem.Model
 {
     public class BallotAccessor : IBallotAccessor
     {
-        //TODO
         public bool AddBallot(Ballot ballot)
         {
-            throw new NotImplementedException();
+            using (var conn = new MySqlConnection(DbConnecter.ConnectionString))
+            {
+                try
+                {
+                    conn.Open();
+                }
+                catch (MySqlException e)
+                {
+                    Console.WriteLine(e + "\nCould not connect to database");
+                    throw;
+                }
+
+                using (var cmd = new MySqlCommand("add_ballot", conn))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+
+                    cmd.Parameters.AddWithValue("v_ballotSerial", ballot.SerialNumber);
+                    cmd.Parameters["@v_ballotSerial"].Direction = ParameterDirection.Input;
+
+                    cmd.Parameters.AddWithValue("v_voterSerial", ballot.Voter.SerialNumber);
+                    cmd.Parameters["@v_voterSerial"].Direction = ParameterDirection.Input;
+
+                    cmd.Parameters.AddWithValue("v_issueSerial", ballot.Issue.SerialNumber);
+                    cmd.Parameters["@v_issueSerial"].Direction = ParameterDirection.Input;
+
+                    cmd.Parameters.AddWithValue("v_choiceNumber", ballot.Choice);
+                    cmd.Parameters["@v_choiceNumber"].Direction = ParameterDirection.Input;
+
+                    cmd.Parameters.Add("v_collision", MySqlDbType.Byte);
+                    cmd.Parameters["@v_collision"].Direction = ParameterDirection.Output;
+
+                    try
+                    {
+                        cmd.ExecuteNonQuery();
+                    }
+                    catch (MySqlException e)
+                    {
+                        Console.WriteLine(e.ErrorCode);
+                        Console.WriteLine(e + "\n\n"
+                            + $@"Could not execute SQL procedure 'add_ballot' with parameters:
+                            ballot serialNumber: '{ballot.SerialNumber}',
+                            voter serialNumber: '{ballot.Voter.SerialNumber}',
+                            issue serialNumber: '{ballot.Issue.SerialNumber}',
+                            ballot choiceNumber: '{ballot.Choice}'");
+                        throw;
+                    }
+                    return Convert.ToBoolean(cmd.Parameters["v_collision"].Value);
+                }
+            }
         }
 
         public List<Ballot> GetBallotsByVoter(string voterSerial)
         {
-            throw new NotImplementedException();
-        }
+            List<Ballot> ballots = new List<Ballot>();
 
-        public List<BallotIssue> GetIssuesVotedOn(string voterSerial)
-        {
-            throw new NotImplementedException();
-        }
-
-        public bool IsSerialInUse(string ballotSerial)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void RemoveBallot(string serial)
-        {
-            throw new NotImplementedException();
-        }
-    }
-}
-    /*
-    public bool AddBallot(Ballot ballot)
-{
             using (var conn = new MySqlConnection(DbConnecter.ConnectionString))
-            {
-                conn.Open();
-            }
-            catch(MySqlException e)
-            {
-                Console.WriteLine(e + "\nCould not connect to database");
-                throw;
-            }
-
-            using (var cmd = new MySqlCommand("add_ballot"))
-            {
-                cmd.CommandType = CommandType.StoredProcedure;
-                /*
-                IN `v_ballotSerial` varchar(9),  
-                IN `v_voterSerial` varchar(9),
-                IN `v_issueSerial` varchar(9),
-                IN `v_choiceNumber` int,
-                OUT `v_collision` bool)
-                cmd.Parameters.AddWithValue("v_ballotSerial", ballot.)
-            }
-      
-    throw new NotImplementedException();
-}
-
-public List<Ballot> GetBallotsByVoter(string voterSerial)
-{
-            
-           CREATE PROCEDURE afetzner.get_voters_ballot(
-    IN `v_voterSerial` varchar(9),
-    IN `v_issueSerial` varchar(9),
-    OUT `v_ballotSerial` varchar(9),
-    OUT `v_choiceNumber` int,
-    OUT `v_choiceTitle` varchar(127))
-BEGIN
-  END
-                
-    using(var conn = new MySqlConnection(DbConnecter.ConnectionString))
             {
                 try
                 {
@@ -92,44 +82,141 @@ BEGIN
                     cmd.CommandType = CommandType.StoredProcedure;
 
                     cmd.Parameters.AddWithValue("v_voterSerial", voterSerial);
-                    cmd.Parameters["v_voterSerial"].Direction = ParameterDirection.Input
+                    cmd.Parameters["v_voterSerial"].Direction = ParameterDirection.Input;
+
+                    cmd.Parameters.Add("v_ballotSerial", MySqlDbType.VarChar);
+                    cmd.Parameters["@v_ballotSerial"].Direction = ParameterDirection.Output;
+
+                    cmd.Parameters.Add("v_choiceNumber", MySqlDbType.Int32);
+                    cmd.Parameters["@v_choiceNumber"].Direction = ParameterDirection.Output;
+
+                    cmd.Parameters.Add("v_choiceTitle", MySqlDbType.VarChar);
+                    cmd.Parameters["@v_choiceTitle"].Direction = ParameterDirection.Output;
+
+                    try
+                    {
+                        cmd.ExecuteNonQuery();
+                    }
+                    catch (MySqlException e)
+                    {
+                        Console.WriteLine(e + "\n" + $@"\nCould not execute SQL procedure get_voters_ballot' with parameters:
+                                                    voterSerial: '{voterSerial}'");
+
+                        throw;
+                    }
                 }
             }
-    throw new NotImplementedException();
-}
+            throw new NotImplementedException();
 
-public List<BallotIssue> GetIssuesVotedOn(string voterSerial)
-{
-    throw new NotImplementedException();
-}
-
-public bool IsSerialInUse(string ballotSerial)
-{
-    using (var conn = new MySqlConnection(DbConnecter.ConnectionString))
-    {
-        try
-        {
-            conn.Open();
-        }
-        catch (MySqlException e)
-        {
-            Console.WriteLine(e + "\nCound not connect to database");
-            throw;
         }
 
-        using (var cmd = new MySqlCommand("check_ballot_serial")
-    
-    CREATE PROCEDURE afetzner.check_ballot_serial(
-    IN `v_ballotSerial` varchar(9),
-    OUT `v_occupied` bool)
-    
-    throw new NotImplementedException();
-}
 
-public void RemoveBallot(string serial)
-{
-    throw new NotImplementedException();
-}
-}
-}
-    */
+
+
+
+        public List<BallotIssue> GetIssuesVotedOn(string voterSerial)
+            {
+                List<BallotIssue> issues = new List<BallotIssue>();
+
+            using (var conn = new MySqlConnection(DbConnecter.ConnectionString))
+            {
+                try
+                {
+                    conn.Open();
+                }
+                catch (MySqlException e)
+                {
+                    Console.WriteLine(e + "\nCould not connect to database");
+                    throw;
+                }
+
+               
+            }
+                throw new NotImplementedException();
+            /*
+            CREATE PROCEDURE afetzner.get_did_voter_participate(
+            IN `v_voterSerial` varchar(9),
+            IN `v_issueSerial` varchar(9),
+            OUT `v_didVote` bool)
+            BEGIN
+            */
+        }
+
+
+
+        public bool IsSerialInUse(string ballotSerial)
+            {
+                using (var conn = new MySqlConnection(DbConnecter.ConnectionString))
+                {
+                    try
+                    {
+                        conn.Open();
+                    }
+                    catch (MySqlException e)
+                    {
+                        Console.WriteLine(e + "\nCound not connect to database");
+                        throw;
+                    }
+
+                    using (var cmd = new MySqlCommand("check_ballot_serial", conn))
+                    {
+                        cmd.CommandType = CommandType.StoredProcedure;
+
+                        cmd.Parameters.AddWithValue("v_ballotSerial", ballotSerial);
+                        cmd.Parameters["@v_ballotSerial"].Direction = ParameterDirection.Input;
+
+                        cmd.Parameters.Add("v_occupied", MySqlDbType.Byte);
+                        cmd.Parameters["@v_occupied"].Direction = ParameterDirection.Input;
+
+                        try
+                        {
+                            cmd.ExecuteNonQuery();
+                        }
+                        catch (MySqlException e)
+                        {
+                            Console.WriteLine(e + "\n" + $@"Could not execute SQL procedure 'check_ballot_serial' with parameters:
+                                                serialNumber: '{ballotSerial}'");
+                            throw;
+                        }
+                        return Convert.ToBoolean(cmd.Parameters["v_occupied"].Value);
+                    }
+                }
+            }
+
+            public void RemoveBallot(string serial)
+            {
+                using (var conn = new MySqlConnection(DbConnecter.ConnectionString))
+                {
+                    try
+                    {
+                        conn.Open();
+                    }
+                    catch (MySqlException e)
+                    {
+                        Console.WriteLine(e + "\nCould not connect to database");
+                        throw;
+                    }
+
+                    using (var cmd = new MySqlCommand("delete_ballot", conn))
+                    {
+                        cmd.CommandType = CommandType.StoredProcedure;
+
+                        cmd.Parameters.AddWithValue("v_ballotSerial", serial);
+                        cmd.Parameters["@v_ballotSerial"].Direction = ParameterDirection.Input;
+
+                        try
+                        {
+                            cmd.ExecuteNonQuery();
+                        }
+                        catch (MySqlException e)
+                        {
+                            Console.WriteLine(e + "\n" + $@"Could not execute SQL procedure 'delete_ballot' with parameters:
+                                                       serialNumber: '{serial}' ");
+
+                            throw;
+                        }
+                    }
+                }
+            }
+        }
+    }
