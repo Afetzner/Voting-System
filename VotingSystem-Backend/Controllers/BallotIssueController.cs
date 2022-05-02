b028cc7afcc5269706f1d9815a0a54379e5d65df
Afetzner <59979332+Afetzner@users.noreply.github.com>
2022-05-02 18:17:41 -0500

    Added shared results viewer ro BallotIssueController

--- VotingSystem-Backend/Controllers/BallotIssueController.cs
+++ VotingSystem-Backend/Controllers/BallotIssueController.cs
@@ -5,52 +5,15 @@ namespace VotingSystem.Controller
 {
     [ApiController]
     public class BallotIssueController : ControllerBase
-    {        
+    {
+        //Shared context for all api calls
+        private ResultViewer resultViewer = SharedResultViewer.ResultViewer;
+
         [HttpGet]
         [Route("api/polls")]
         public List<BallotIssue> GetBallotIssues()
-        {
-            /*
-            List<BallotIssue> list = new List<BallotIssue>();
-            BallotIssue issue1 = new BallotIssue.BallotIssueBuilder()
-                .WithSerialNumber("A12345678")
-                .WithStartDate(new DateTime(2022, 3, 12))
-                .WithEndDate(new DateTime(2022, 8, 5))
-                .WithTitle("Test v. Test")
-                .WithDescription("test")
-                .WithOptions("option1", "option2")
-                .Build();
-            list.Add(issue1);
-            BallotIssue issue2 = new BallotIssue.BallotIssueBuilder()
-                .WithSerialNumber("B12345678")
-                .WithStartDate(new DateTime(2022, 3, 12))
-                .WithEndDate(new DateTime(2022, 7, 5))
-                .WithTitle("Test v. Test")
-                .WithDescription("test")
-                .WithOptions("option1", "option2")
-                .Build();
-            list.Add(issue2);
-            BallotIssue issue3 = new BallotIssue.BallotIssueBuilder()
-                .WithSerialNumber("B12345678")
-                .WithStartDate(new DateTime(2022, 3, 12))
-                .WithEndDate(new DateTime(2022, 7, 5))
-                .WithTitle("Test v. Test")
-                .WithDescription("test")
-                .WithOptions("option1", "option2")
-                .Build();
-            list.Add(issue3);
-            BallotIssue issue4 = new BallotIssue.BallotIssueBuilder()
-                .WithSerialNumber("A12345678")
-                .WithStartDate(new DateTime(2021, 3, 12))
-                .WithEndDate(new DateTime(2021, 8, 5))
-                .WithTitle("Test v. Test")
-                .WithDescription("test")
-                .WithOptions("option1", "option2")
-                .Build();
-            list.Add(issue4);
-            return list;
-            */
-            return BallotIssue.Accessor.GetBallotIssues();
+        {   
+            return resultViewer.GetBallotIssues();
         }
     }
 }
