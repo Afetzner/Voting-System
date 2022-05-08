ce774312fe205eb0a19830ff19ee27560db16365
akrestovsky <59978361+akrestovsky@users.noreply.github.com>
2022-05-08 00:36:10 -0500

    method to post ballot, not yet tested
    PostBallot

---
+++ VotingSystem-Backend/Controllers/APIs/BallotController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using VotingSystem.Model;
+
+namespace VotingSystem.Controller
+{
+    [ApiController]
+    public class BallotController : ControllerBase
+    {
+        private static readonly ResultCacheManager cache = ResultCacheManager.SharedCacheManager;
+
+        [HttpPost]
+        [Route("api/ballot")]
+        public Ballot PostBallot(string issueSerial, string voterSerial, int choice, )
+        {
+             Ballot ballot = new Ballot.Builder()
+                        .WithIssue(issueSerial)
+                        .WithVoter(voterSerial)
+                        .WithChoice(choice)
+                        .WithSerialNumber(Ballot.Accessor.GetSerial())
+                        .Build();
+                    Ballot.Accessor.AddBallot(ballot);
+/*
+            BallotManager.AddBallot(ballot); //not sure if use this or the one above
+*/
+            return ballot;
+        }
+    }
+}
